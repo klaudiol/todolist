1c3317f6b78de2ecfd8c15235575c5e847728ccf
klaudiol <klokner@etfos.hr>
2025-08-03 16:32:20 +0200

    Update todo.h

--- todo.h
+++ todo.h
@@ -1,41 +1,40 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-
+// todo.h
+#define _CRT_SECURE_NO_WARNINGS
 #ifndef TODO_H
 #define TODO_H
 
-#define MAX_NAZIV 100
-#define MAX_OPIS 200
 #define NAZIV_DATOTEKE "zadaci.txt"
 
-typedef enum {
-    NIZAK = 1,
-    SREDNJI,
-    VISOK
-} Prioritet;
+#include <stdio.h>
 
+// Enum za prioritet zadatka
+typedef enum { NIZAK = 1, SREDNJI, VISOK } Prioritet;
+
+// Struktura za zadatak
 typedef struct Zadatak {
     int id;
-    char naziv[MAX_NAZIV];
-    char opis[MAX_OPIS];
+    char naziv[50];
+    char opis[100];
     Prioritet prioritet;
     struct Zadatak* sljedeci;
 } Zadatak;
 
-// Pokazivač na funkciju za usporedbu
-typedef int (*usporedbaFunkcija)(Zadatak*, Zadatak*);
+// Globalni pokazivač na početak liste
+extern Zadatak* glava;
 
+// Deklaracije funkcija
+void ucitajIzDatoteke();
+void spremiUDatoteku();
 void dodajZadatak();
 void prikaziZadatke();
-void ispisiRekurzivno(Zadatak* z);
-Zadatak* nadjiZadatak(int id);
 void azurirajZadatak();
 void obrisiZadatak();
 void umetniNaPoziciju();
-void sortirajZadatke(usporedbaFunkcija f);
-int usporediPoPrioritetu(Zadatak* a, Zadatak* b);
+int usporediZadatke(const void*, const void*);
+void sortirajZadatke(int (*)(const void*, const void*));
 void traziZadatak();
-void spremiUDatoteku();
-void ucitajIzDatoteke();
 void oslobodiMemoriju();
+void preimenujDatoteku();
+void obrisiDatoteku();
 
 #endif
